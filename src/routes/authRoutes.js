167214b4b9c01aeacbd103365edb3be93ec40690
nimofx <nimofx.llc@gmail.com>
2026-05-02 17:05:39 +0530

    fix route imports

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, login } = require("../controllers/authcontroller");
+const { register, login } = require("../controllers/authController");
 
 const router = express.Router();
 
